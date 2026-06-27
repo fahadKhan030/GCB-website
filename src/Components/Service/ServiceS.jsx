094d5d009093a9f3ec9ebe2dd8c3d68f782e3a20
Fahad Khan <fahdkhn30@gmail.com>
2026-06-27 17:01:42 +0500

    update the Ui

--- src/Components/Service/ServiceS.jsx
+++ src/Components/Service/ServiceS.jsx
@@ -5,6 +5,7 @@ const Service = () => {
   return (
     <div  className="max-w-[1400px] mx-auto justify-center ">
         <Expertise/>
+        <h1>how are you in this time</h1>
     </div>
   )
 }
