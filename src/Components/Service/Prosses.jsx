c92d724e6b2cef60bba96503c825dceae2631e83
fahadkhan030 <fahdkhn30@gmail.com>
2026-07-14 23:26:42 +0500

    Refactor Prosses component to simplify structure and enhance clarity of process description

--- src/Components/Service/Prosses.jsx
+++ src/Components/Service/Prosses.jsx
@@ -1,84 +1,17 @@
 import React from 'react'
 
 const Prosses = () => {
-  
-    const steps = [
-  {
-    number: "1",
-    title: "Consultation",
-    desc: "Defining project goals, constraints and objectives."
-  },
-  {
-    number: "2",
-    title: "Analysis & Design",
-    desc: "Topographic surveying and architectural planning."
-  },
-  {
-    number: "3",
-    title: "Technical Development",
-    desc: "Engineering calculations and planning."
-  },
-  {
-    number: "4",
-    title: "Execution",
-    desc: "On-site management and safety."
-  }
-];
-
   return (
-      <section className="max-w-6xl mx-auto py-24">
-      <div className="relative">
-
-        {/* Center Line */}
-        <div className="absolute left-1/2 top-0 h-full w-[2px] -translate-x-1/2 bg-green-700"></div>
-
-        {steps.map((step, index) => (
-          <div
-            key={index}
-            className="relative grid grid-cols-2 items-center mb-10"
-          >
-            {/* Left Content */}
-            <div
-              className={`${
-                index % 2 === 0
-                  ? "text-right pr-10 "
-                  : "opacity-0 pointer-events-none"
-              }`}
-            >
-              <h3 className="text-xl font-bold text-green-700">
-                {step.title}
-              </h3>
-
-              <p className="mt-3 text-gray-400">
-                {step.desc}
-              </p>
-            </div>
-
-            {/* Right Content */}
-            <div
-              className={`${
-                index % 2 !== 0
-                  ? "pl-20"
-                  : "opacity-0 pointer-events-none"
-              }`}
-            >
-              <h3 className="text-xl font-bold text-green-700">
-                {step.title}
-              </h3>
-
-              <p className="mt-3 text-gray-400">
-                {step.desc}
-              </p>
-            </div>
-
-            {/* Circle */}
-            <div className="absolute left-1/2 -translate-x-1/2 w-16 h-16 rounded-full bg-green-700 flex items-center justify-center text-white font-bold text-2xl">
-              {step.number}
-            </div>
-          </div>
-        ))}
+    <div className= "relative mx-auto text-center">
+      <div>
+        <h1>Our Seamless Process</h1>
+        <span>A rigorous workflow designed to ensure precision at every milestone.</span>
       </div>
-    </section>
+
+      <div className='grid grid-cols-2 '>
+        <div className=' absolute left-1/2 w-1  bg-primary '></div>
+      </div>
+    </div>
   )
 }
 
