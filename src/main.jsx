4c7450990532c8d1be1d97c4d75d0099b8f94dc4
fahadkhan030 <fahdkhn30@gmail.com>
2026-08-03 21:06:26 +0500

    Fix case sensitivity for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App";
 // import pages
-import Home from "../src/Pages/Home.jsx";
-import About from "../src/Pages/About.jsx";
-import Service from "../src/Pages/Service.jsx";
-import Project from "../src/Pages/Project.jsx";
-import Contact from "../src/Pages/Contact.jsx";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Service from "./Pages/Service.jsx";
+import Project from "./Pages/Project.jsx";
+import Contact from "./Pages/Contact.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
